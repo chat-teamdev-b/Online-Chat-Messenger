c5e6cc33312f53b7973afd0bbfce87d6d31975ad
anko1055 <msk.1055@icloud.com>
2024-09-25 21:45:11 +0900

    stage2 udp

--- client.py
+++ client.py
@@ -10,17 +10,17 @@ class TCPClient:
         self.info = {}
     
     def protocol_header(self, room_name_bytes_len, operation, state, user_name_bytes_len):
-        return room_name_bytes_len.to_bytes(1, "big") + operation.to_bytes(1, "big") + state.to_bytes(1,"big") + user_name_bytes_len.to_bytes(29, "big")
+        return room_name_bytes_len.to_bytes(1, "big") + operation.to_bytes(1, "big") + state.to_bytes(1, "big") + user_name_bytes_len.to_bytes(1, "big")
 
     def input_user_name(self):
         user_name = input("ユーザー名を入力してください: ")
         self.info["user_name"] =  user_name
         user_name_bytes = user_name.encode('utf-8')
-        if len(user_name_bytes) > 2**29:
-            print("ルーム名は2**29バイト以下にしてください:")
-            return self.input_room_name()
+        if len(user_name_bytes) > 2**8:
+            print("ユーザー名は2**8バイト以下にしてください:")
+            return self.input_user_name()
         elif len(user_name_bytes) == 0:
-            return self.input_room_name()
+            return self.input_user_name()
         else:
             return user_name_bytes
         
@@ -61,7 +61,7 @@ class TCPClient:
         room_name_bytes_len = len(room_name_bytes)
         header = self.protocol_header(room_name_bytes_len, operation, 1, user_name_bytes_len)
         body = room_name_bytes + user_name_bytes
-        self.sock.send(header+body)
+        self.sock.send(header + body)
     
     def receive_message(self):
         self.sock.settimeout(10)
@@ -73,7 +73,7 @@ class TCPClient:
                     print(self.info)
                 else:
                     break
-        except(TimeoutError):
+        except TimeoutError:
             print('Socket timeout, ending listening for server messages')
         finally:
             print('closing socket')
@@ -92,17 +92,40 @@ class TCPClient:
         return self.info
 
 
-# class UDPClient:
+class UDPClient:
+    def __init__(self, server_address, server_port, info):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_address = server_address
+        self.server_port = server_port
+        self.user_name = info['user_name']
+        print("please enter message")
 
+    def receive_messages(self):
+        while True:
+            full_message, server = self.sock.recvfrom(4096)
+            user_name_length = full_message[0]
+            user_name = full_message[1:user_name_length + 1].decode("utf-8")
+            message = full_message[user_name_length + 1:].decode("utf-8")
+            print("\033[1A") 
+            print(f'{user_name}: {message}')
+    def start(self):
+        threading.Thread(target=self.receive_messages, daemon=True).start()
+        while True:
+            message = input('')
+            print("\033[1A", end="") 
+            print(f'{self.user_name}: {message}')
+            message_bytes = message.encode('utf-8')
+            full_message_bytes = bytes([len(self.user_name)]) + self.user_name.encode('utf-8') + message_bytes
+            self.sock.sendto(full_message_bytes, (self.server_address, self.server_port))
 
 
 if __name__ == "__main__":
     server_address = '0.0.0.0'
     tcp_server_port = 9001
-    # udp_server_port = 9002
+    udp_server_port = 9002
 
     tcp_client = TCPClient(server_address, tcp_server_port)
     info = tcp_client.start()
 
-    # udp_client = UDPClient(server_address, udp_server_port, info)
-    # udp_client.start()
+    udp_client = UDPClient(server_address, udp_server_port, info)
+    udp_client.start()
--- server.py
+++ server.py
@@ -80,20 +80,69 @@ class ChatRoom:
         print(self.chat_rooms)
 
 
-# class UDPServer:
+# UDPサーバークラスの定義
+class UDPServer:
+    def __init__(self, host, port, chat_room):
+        self.server_address = (host, port)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+        self.chat_room = chat_room
+        self.sock.bind(self.server_address)
+        self.clients = {}
+        self.client_last_active = {}
+
+    def update_client_last_message_sent_time(self, client_address):
+        self.client_last_active[client_address] = time.time()
+
+    def remove_inactive_clients(self):
+        while True:
+            current_time = time.time()
+            for client_address in list(self.client_last_active.keys()):
+                if current_time - self.client_last_active[client_address] > 60:
+                    print(f"Removing inactive client: {client_address}")
+                    del self.client_last_active[client_address]
+                    del self.clients[client_address]
+            time.sleep(30)
 
 
+# メッセージをリレー
+    def relay_message(self, full_message, sender_address, username):
+        for client_address in self.clients.keys():
+            if client_address != sender_address:
+                sent = self.sock.sendto(full_message, client_address)
+                print('sent {} bytes back to {}'.format(sent, client_address))
+                
+    def start(self):
+        threading.Thread(target=self.remove_inactive_clients, daemon=True).start()
+        while True:
+            print('\nwaiting to receive message')
+            full_message, client_address = self.sock.recvfrom(4096)
+            room_name_size = full_message[0]
+            token_size = full_message[1]
+            room_name = full_message[2: 2+room_name_size].decode()
+            token = full_message[2+room_name_size: 2+room_name_size+token_size]
+            message = full_message[2+room_name_size+token_size:].decode()
+            print('received {} bytes from {}'.format(len(message), self.user_name))
+            print(message)
+            self.update_client_last_message_sent_time(client_address)
+            if message:
+                self.relay_message(full_message, client_address, self.user_name) 
 
+    def handle_message(self):
+        print("UDP server listening for messages...")
+        while True:
+            data, addr = self.sock.recvfrom(4096)
+            print(f"Received message from {addr}: {data.decode('utf-8')}")
+            
 if __name__ == "__main__":
     server_address = '0.0.0.0'
     tcp_server_port = 9001
-    # udp_server_port = 9002
+    udp_server_port = 9002
     chat_room = ChatRoom()
     tcp_server = TCPServer(server_address, tcp_server_port)
-    # udp_server = UDPServer(server_address, udp_server_port, chat_room)
+    udp_server = UDPServer(server_address, udp_server_port, chat_room)
     thread_tcp_server = threading.Thread(target=tcp_server.handle_message)
-    # thread_udp_server = threading.Thread(target=udp_server.handle_message)
+    thread_udp_server = threading.Thread(target=udp_server.handle_message)
     thread_tcp_server.start()
-    # thread_udp_server.start()
+    thread_udp_server.start()
     thread_tcp_server.join()
-    # thread_udp_server.join()
+    thread_udp_server.join()
